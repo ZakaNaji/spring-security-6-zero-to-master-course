57a9da1818fcabec1f0345a73b680a8b6319e1be
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-04 17:02:44 +0100

    customize the entry point error body

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/exceptionhandling/CustomBasicAuthenticationEntryPoint.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/exceptionhandling/CustomBasicAuthenticationEntryPoint.java
@@ -3,7 +3,9 @@ package com.eazybytes.springsecsection1.exceptionhandling;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.server.Encoding;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -13,6 +15,18 @@ public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryP
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.addHeader("Custom-Error", "Auth Failed bc of something");
-        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        String message = (authException != null && authException.getMessage() != null)? authException.getMessage() : "UNAUTHORIZED";
+        String path = request.getRequestURI();
+        String respJson = String.format("""
+                {
+                    "message": "%s",
+                    "path": "%s"
+                }
+                """, message, path);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(Encoding.DEFAULT_CHARSET.displayName());
+        response.getWriter().write(respJson);
     }
 }
