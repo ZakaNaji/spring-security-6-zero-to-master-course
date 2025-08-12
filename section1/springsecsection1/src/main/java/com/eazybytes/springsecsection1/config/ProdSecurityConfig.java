25b7a19236c9cc674f9b857bace8eeaaf2e9e578
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-12 18:37:49 +0100

    add our custom filter after the existing Csrf Filter

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/config/ProdSecurityConfig.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/config/ProdSecurityConfig.java
@@ -2,6 +2,7 @@ package com.eazybytes.springsecsection1.config;
 
 import com.eazybytes.springsecsection1.exceptionhandling.CustomAccessDeniedHandler;
 import com.eazybytes.springsecsection1.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import com.eazybytes.springsecsection1.filters.CsrfTokenLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
 @Profile("prod")
@@ -21,7 +23,7 @@ public class ProdSecurityConfig {
         return http.authorizeHttpRequests(req ->
                 req.requestMatchers("/welcome","/contact", "/notices", "/error", "/register", "/invalidSession").permitAll().anyRequest().authenticated())
                 .httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()))
-                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
                 .exceptionHandling(exp -> exp
                         .authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint())// global exception handling via the entry point
                         .accessDeniedHandler(new CustomAccessDeniedHandler()))
