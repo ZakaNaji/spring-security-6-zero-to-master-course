56d991fb466e9fdc966c55e220311427489cad5a
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-12 18:37:10 +0100

    a custom filter that log the genrated csrf token. the token will get generated after a GET request, so we retrieve from a req attribute called _csrf and log it to consol

---
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/filters/CsrfTokenLogger.java
@@ -0,0 +1,19 @@
+package com.eazybytes.springsecsection1.filters;
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.csrf.CsrfToken;
+
+import java.io.IOException;
+
+@Slf4j
+public class CsrfTokenLogger implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        CsrfToken csrfToken = (CsrfToken) servletRequest.getAttribute("_csrf");
+
+        log.warn("CSRF TOKEN: " + csrfToken.getToken());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
