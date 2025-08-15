c60be891f6215ad6d9d04f0bd3e7d1745039409d
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-15 16:28:43 +0100

    add ContactRepository

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/repository/ContactRepository.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/repository/ContactRepository.java
@@ -1,9 +1,9 @@
-package com.eazybytes.repository;
+package com.eazybytes.springsecsection1.repository;
 
+import com.eazybytes.springsecsection1.model.Contact;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.eazybytes.model.Contact;
 
 @Repository
 public interface ContactRepository extends CrudRepository<Contact, String> {
