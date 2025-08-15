80564a68894960161a820d02e940e700b9089900
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-15 16:29:07 +0100

    add NoticeRepository

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/repository/NoticeRepository.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/repository/NoticeRepository.java
@@ -1,17 +1,17 @@
-package com.eazybytes.repository;
+package com.eazybytes.springsecsection1.repository;
 
 import java.util.List;
 
+import com.eazybytes.springsecsection1.model.Notice;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.eazybytes.model.Notice;
 
 @Repository
 public interface NoticeRepository extends CrudRepository<Notice, Long> {
 	
-	@Query(value = "from Notice n where CURDATE() BETWEEN noticBegDt AND noticEndDt")
+	@Query(value = "from Notice n where CURDATE() BETWEEN n.noticBegDt AND n.noticEndDt")
 	List<Notice> findAllActiveNotices();
 
 }
