2d094647f5f60db7a2b73afeed5ac7b8740fa943
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-15 16:12:19 +0100

    add Customer model

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/model/Customer.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/model/Customer.java
@@ -1,52 +1,36 @@
 package com.eazybytes.springsecsection1.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "customer")
+@Getter @Setter
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "customer_id")
+    private long id;
+
+    private String name;
 
     private String email;
 
-    @Column(name = "pwd")
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @Enumerated(value = EnumType.STRING)
-    private Role role;
+    private String role;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
+    @Column(name = "create_dt")
+    @JsonIgnore
+    private Date createDt;
 }
