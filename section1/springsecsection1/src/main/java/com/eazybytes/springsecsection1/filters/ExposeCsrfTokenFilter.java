74d94df0f9707b5287722b0360905737b185347b
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-17 18:37:18 +0100

    add ExposeCsrfTokenFilter that will force CsrfFilter to generated a csrf token after basic auth

---
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/filters/ExposeCsrfTokenFilter.java
@@ -0,0 +1,22 @@
+package com.eazybytes.springsecsection1.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class ExposeCsrfTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (csrfToken != null) {
+            String token = csrfToken.getToken();
+            response.setHeader("X-CSRF-TOKEN", token);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
