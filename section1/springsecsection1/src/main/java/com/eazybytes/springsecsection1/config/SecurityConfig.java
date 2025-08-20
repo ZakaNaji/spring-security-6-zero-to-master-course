c65ce0905b4cc1bdb1e491e3ceb37ab58b6cfa66
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-20 18:14:03 +0100

    add csrf config to default profile config

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/config/SecurityConfig.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.eazybytes.springsecsection1.config;
 
+import com.eazybytes.springsecsection1.filters.CsrfTokenLogger;
+import com.eazybytes.springsecsection1.filters.ExposeCsrfTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @Profile("!prod")
@@ -20,7 +25,10 @@ public class SecurityConfig {
                 req.requestMatchers("/welcome","/contact", "/notices", "/error", "/register").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrfConfig -> csrfConfig
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+                .addFilterAfter(new CsrfTokenLogger(), BasicAuthenticationFilter.class)
                 .build();
     }
 
